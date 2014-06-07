1a6dd28660ac3834bd09d79463974ffdf076a99b
Frank Bouwens <frankkie12345@gmail.com>
2014-06-07 17:12:35 +0200

    Fixed Wave 10

--- src/nl/fluffikens/pony/waves/Wave10.java
+++ src/nl/fluffikens/pony/waves/Wave10.java
@@ -1,6 +1,6 @@
 package nl.fluffikens.pony.waves;
 
-import nl.fluffikens.pattern.NullPattern;
+import nl.fluffikens.pattern.AZPattern;
 import nl.fluffikens.pony.Pony;
 
 /**
@@ -8,38 +8,67 @@ import nl.fluffikens.pony.Pony;
  * @author FrankkieNL
  */
 public class Wave10 extends AbstractWave {
-     public Wave10() {
-        this.names = new String[]
-        {
-            "Trixie Lulamoon", "Green Jewel", "Sunny Rays",
-            "Lotus Blossom", "Ribbon Wishes", "Snailsquirm",
-            "Twilight Sparkle (Princess)", "Peachy Sweet II",
-            "Junebug", "Sunshower Raindrops", "Gilda the Griffon",
-            "Strawberry Sunrise", "Sassaflash", "Royal Riff",
-            "Lemon Hearts", "Applejack", "Diamond Mint",
-            "Flower Wishes", "Fluttershy II", "Mosely Orange",
-            "Pinkie Pie", "Rainbow Swoop", "Rainbowshine", "Snipsy Snap"                
-            
+
+    public Wave10() {
+        this.names = new String[]{
+            "Applejack",
+            "Trixie Lulamoon",
+            "Fluttershy",
+            "Pinkie Pie",
+            "Green Jewel",
+            "Rainbowshine",
+            "Sunny Rays",
+            "Lotus Blossom",
+            "Ribbon Wishes",
+            "Snailsquirm",
+            "Diamond Mint",
+            "Twilight Sparkle (Princess)",
+            "Peachy Sweet",
+            "Flower Wishes",
+            "Snipsy Snap",
+            "Mosely Orange",
+            "June Bug",
+            "Rainbow Swoop",
+            "Sunshower Raindrops",
+            "Gilda The Griffon",
+            "Strawberry Sunrise",
+            "Sassaflash",
+            "Royal Riff",
+            "Lemon Hearts",           
+
         };
 
-        this.descriptions = new String[][]
-        {
-            {"02", "glitter"}, {"05", "glitter"}, {"07", "glitter"}, 
-            {"08", "glitter"}, {"09", "glitter"}, {"10", "glitter"}, 
-            {"12", "glitter"}, {"13", "glitter"}, {"17", "glitter"}, 
-            {"19", "glitter"}, {"20", "glitter"}, {"21", "glitter"}, 
-            {"22", "glitter"}, {"23", "glitter"}, {"24", "glitter"}, 
-            {"??", "glitter"}, {"??", "glitter"}, {"??", "glitter"}, 
-            {"??", "glitter"}, {"??", "glitter"}, {"??", "glitter"}, 
-            {"??", "glitter"}, {"??", "glitter"}, {"??", "glitter"}, 
-            {"??", "glitter"}, 
-        };
+        this.descriptions = new String[][]{
+            {"01", "glitter"}, 
+            {"02", "glitter"}, 
+            {"03", "glitter"},
+            {"04", "glitter"}, 
+            {"05", "glitter"}, 
+            {"06", "glitter"},
+            {"07", "glitter"},
+            {"08", "glitter"},
+            {"09", "glitter"},
+            {"10", "glitter"},
+            {"11", "glitter"}, 
+            {"12", "glitter"},
+            {"13", "glitter"}, 
+            {"14", "glitter"}, 
+            {"15", "glitter"},
+            {"16", "glitter"},
+            {"17", "glitter"}, 
+            {"18", "glitter"},
+            {"19", "glitter"},
+            {"20", "glitter"}, 
+            {"21", "glitter"},
+            {"22", "glitter"}, 
+            {"23", "glitter"},
+            {"24", "glitter"},};
 
         this.wave = 10;
         this.waveName = "" + this.wave;
         this.ponies = new Pony[names.length];
-        
-        this.patterns.add(new NullPattern());
+
+        this.patterns.add(new AZPattern());
 
         this.init();
 
