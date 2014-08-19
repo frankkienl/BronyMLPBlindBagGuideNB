17680914ee1848f65917b9ed3ffd00f3c79cd5b9
Frank Bouwens <frankkie12345@gmail.com>
2014-08-19 21:52:02 +0200

    text fixes
    Fixed padding in credits and tutorial
    Fixed spaces for text by removing XML-formatting.

--- gen/R.java.d
+++ gen/R.java.d
@@ -16,7 +16,6 @@ E:\Dropbox\NetbeansGithub\BronyMLPBlindBagGuide\res\menu\main.xml \
 E:\Dropbox\NetbeansGithub\BronyMLPBlindBagGuide\res\values\dimens.xml \
 E:\Dropbox\NetbeansGithub\BronyMLPBlindBagGuide\res\values\strings.xml \
 E:\Dropbox\NetbeansGithub\BronyMLPBlindBagGuide\res\values\styles.xml \
-E:\Dropbox\NetbeansGithub\BronyMLPBlindBagGuide\res\values\values-fr \
 E:\Dropbox\NetbeansGithub\BronyMLPBlindBagGuide\res\values-nl\strings.xml \
 E:\Dropbox\NetbeansGithub\BronyMLPBlindBagGuide\res\values-sw600dp\dimens.xml \
 E:\Dropbox\NetbeansGithub\BronyMLPBlindBagGuide\res\values-sw720dp-land\dimens.xml \
